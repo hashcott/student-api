6f6972e3afd74a99aca990b02fe7a0c0b978998a
Nguyen Duc Hanh <1851061469@e.tlu.edu.vn>
2020-08-21 14:59:43 +0700

    add util support generate timeline

--- pages/StudentTimeTable.js
+++ pages/StudentTimeTable.js
@@ -1,5 +1,6 @@
 const delay = require("delay");
 const StudentTimeTableSL = require("../selectors/StudentTimeTable");
+const { generateTimelineByDay, generateTimeline } = require("../util/timeline");
 
 module.exports = async (browser, term) => {
   const page = await browser.newPage();
@@ -9,15 +10,22 @@ module.exports = async (browser, term) => {
   await page.type(StudentTimeTableSL.term, term);
   await page.waitForSelector(StudentTimeTableSL.table);
   const timeTable = await page.evaluate(() => {
-    let timeTableHTML = [
-      ...document.querySelectorAll("#gridRegistered tr"),
-    ].slice(1, 11);
+    // Lưu lại dữ liệu về lịch học các môn
     let timeTable = [];
+
+    //Regex bóc dữ liệu ngày tháng và thời gian học
     const data_pattern = /(.+?) đến (.+?):/;
     const time_pattern = /Thứ ([0-9]) tiết ([0-9,]+?) \((.+?)\)/;
 
+    // Bóc dữ liệu html lịch học từng môn
+    let timeTableHTML = [
+      ...document.querySelectorAll("#gridRegistered tr"),
+    ].slice(1, 11);
+
     for (let html of timeTableHTML) {
       let subjectHTML = [...html.querySelectorAll("td")];
+
+      // Chuyển dữ liệu từng môn hoc về dạng text
       let [
         STT,
         lopHocPhan,
@@ -31,6 +39,7 @@ module.exports = async (browser, term) => {
         ghiChu,
       ] = subjectHTML.map((sj) => sj.innerText.trim());
 
+      // Lưu lại dữ liệu lịch học từng môn
       timeTable.push({
         STT,
         lopHocPhan,
@@ -44,6 +53,8 @@ module.exports = async (browser, term) => {
         ghiChu,
       });
     }
+
+    /* Chuyển dữ liệu thời gian từng môn học về dạng thời gian theo từng giai đoạn học  */
     timeTable.forEach((data, i) => {
       let getTimePhase = data.thoiGian.split("Từ");
       getTimePhase = getTimePhase.filter((time) => time != "");
@@ -64,8 +75,9 @@ module.exports = async (browser, term) => {
       }
       timeTable[i].thoiGian = phases;
     });
-
+    console.log(timeTable);
     return timeTable;
   });
-  // await page.close();
+  await page.close();
+  return timeTable;
 };
---
+++ util/timeline.js
@@ -0,0 +1,72 @@
+const moment = require("moment-timezone");
+const _ = require("lodash");
+const periodBoard = require("./periodBoard");
+moment.tz.setDefault("Asia/Ho_Chi_Minh");
+moment.locale("vi-VN");
+
+const parseTime = (time) => moment(time, "DD/MM/YYYY");
+
+const generateTimestamps = (start, end, day) => {
+  let timeLine = [];
+  // Set ngày bắt đầu
+  start.weekday(day);
+  // Kiểm tra ngày bắt đầu còn nhỏ hơn ngày kết thúc không , nếu có thêm vào timeline
+  while (start.isSameOrBefore(end)) {
+    timeLine.push(start.clone());
+
+    //Tăng thêm 1 tuần để kiểm tra tiếp
+    start.add(1, "week");
+  }
+  return timeLine;
+};
+
+//Chuyển thời gian sang thời gian học và kết thúc từng môn
+const generateClasses = (timeRanges, startPeriod, endPeriod) => {
+  return timeRanges.map((timeStamps) => ({
+    start: timeStamps
+      .clone()
+      .hour(periodBoard[startPeriod].start.hour)
+      .minute(periodBoard[startPeriod].start.minute),
+    end: timeStamps
+      .clone()
+      .hour(periodBoard[endPeriod].end.hour)
+      .minute(periodBoard[endPeriod].end.minute),
+  }));
+};
+
+// Tạo time line
+const generateTimeline = (subjects) => {
+  let timelines = [];
+  for (let subject of subjects) {
+    for (let phase of subject.thoiGian) {
+      for (let range of phase.ranges) {
+        let timeline = generateClasses(
+          generateTimestamps(
+            parseTime(phase.start),
+            parseTime(phase.end),
+            parseInt(range.day) - 2
+          ),
+          range.periods[0],
+          range.periods.pop()
+        );
+        timeline.forEach((timestamp) => {
+          let data = { timestamp, ...subject, ...range };
+          delete data.thoiGian;
+          delete data.STT;
+          timelines.push(data);
+        });
+      }
+    }
+  }
+  timelines.sort((first, last) => first.timestamp.start - last.timestamp.start);
+  return timelines;
+};
+//
+const generateTimelineByDay = (timelines) => {
+  let days = _.groupBy(timelines, (timeline) => {
+    return timeline.timestamp.start.startOf("day");
+  });
+  return days;
+};
+
+module.exports = { generateTimeline, generateTimelineByDay };
